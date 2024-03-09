f5a4a3fba5cc32fd38ac1f86590ae08b4de64165
Henry-Hiles <henry@henryhiles.com>
2024-03-09 16:26:24 -0500

    Readd spawnSync

--- cli.ts
+++ cli.ts
@@ -1,5 +1,19 @@
-import { ExecOptions, exec } from "child_process"
+import { ExecOptions, SpawnOptions, exec, spawn } from "child_process"
 import { promisify } from "util"
+
+export const spawnAsync = (
+    cmd: string,
+    options: SpawnOptions = {
+        stdio: "inherit",
+    }
+): Promise<void> =>
+    new Promise((resolve, reject) => {
+        const cmdParts = cmd.split(" ")
+        const cp = spawn(cmdParts[0], cmdParts.slice(1), options)
+
+        cp.on("error", reject)
+        cp.on("close", resolve)
+    })
 
 export const execAsync = async (
     cmd: string,
