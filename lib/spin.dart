bd90a8c41f3d2e2417a5e84126bb357c67e86fb7
Henry-Hiles <henry@henryhiles.com>
2024-08-03 16:26:10 -0400

    Colored spinners

--- lib/spin.dart
+++ lib/spin.dart
@@ -1,13 +1,14 @@
 import 'package:cli_spin/cli_spin.dart';
+import 'package:io/ansi.dart';
 
 Future<T> spin<T>(String message, Future<T> future) async {
   final spinner = CliSpin(text: message).start();
   try {
     final result = await future;
-    spinner.success();
+    spinner.success(green.wrap(message));
     return result;
   } catch (error, stack) {
-    spinner.fail("$error\n\n$stack");
+    spinner.fail(red.wrap("An error occurred: $error\n\nStacktrace: $stack"));
     rethrow;
   }
 }
