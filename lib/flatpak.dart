883f060324af5b492ba64d2b0861fd5909d72230
Henry-Hiles <henry@henryhiles.com>
2024-08-04 11:32:37 -0400

    Fix args

--- lib/flatpak.dart
+++ lib/flatpak.dart
@@ -2,9 +2,9 @@ import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import "cli.dart" as cli;
 
 abstract class Flatpak {
-  static Future<String> execNonInteractive(String cmd, IList<String> args) =>
-      exec(cmd, args.add("--noninteractive"));
+  static Future<String> execNonInteractive(IList<String> args) =>
+      exec(args.add("--noninteractive"));
 
-  static Future<String> exec(String cmd, IList<String> args) async =>
-      cli.exec("flatpak $cmd", args.add("--system"));
+  static Future<String> exec(IList<String> args) async =>
+      cli.exec("flatpak", args.add("--system"));
 }
