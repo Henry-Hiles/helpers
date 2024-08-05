313b7995762f5713a82ac16cc2149849df652ecf
Henry-Hiles <henry@henryhiles.com>
2024-08-05 13:09:43 -0400

    Use vars

--- lib/paths.dart
+++ lib/paths.dart
@@ -2,13 +2,13 @@ import "dart:io";
 import "package:path/path.dart";
 import "package:xdg_directories/xdg_directories.dart";
 
-Directory getConfigDir() => Directory(join(
-      configHome.absolute.path,
-      "quados",
-    ));
+final configDir = Directory(join(
+  configHome.absolute.path,
+  "quados",
+));
 
-File getSchemaFile() => File(join(cacheHome.absolute.path, "packages.lock"))
+final schemaFile = File(join(cacheHome.absolute.path, "schema.quad.yaml"))
   ..createSync(recursive: true);
 
-File getConfigFile() =>
-    File(join(getConfigDir().path, "config.dart"))..createSync(recursive: true);
+final configFile = File(join(configDir.path, "config.dart"))
+  ..createSync(recursive: true);
